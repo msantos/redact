7a0d84d45a69da331996137b9007a44cf13b1c3f
Michael Santos <michael.santos@gmail.com>
2024-04-21 08:33:58 -0400

    redact: masked example

--- pkg/redact/example_test.go
+++ pkg/redact/example_test.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"codeberg.org/msantos/redact/pkg/redact"
+	"codeberg.org/msantos/redact/pkg/redact/overwrite"
 )
 
 func ExampleOpt_Redact() {
@@ -19,4 +20,20 @@ regex = '''\$(?:[a-zA-Z0-9]+)\$([^\s:]+)'''
 	}
 	fmt.Println(redacted)
 	// Output: root:$6$**REDACTED**:18515:0:99999:7:::
+}
+
+func ExampleOpt_Redact_mask() {
+	red := redact.New(redact.WithRules(`[[rules]]
+id = "crypt-password-hash"
+description = "Detected a password hash"
+regex = '''\$(?:[a-zA-Z0-9]+)\$([^\s:]+)'''
+`),
+		redact.WithOverwrite(overwrite.Mask),
+	)
+	redacted, err := red.Redact("root:$6$d468dc01f1cd655d$1c0a188389f4db6399265080815ac488ea65c3295a18d2d7da3ce5e8ef082362adeedec9b69.9704d4d188:18515:0:99999:7:::")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(redacted)
+	// Output: root:$6$*******************************************************************************************************:18515:0:99999:7:::
 }
