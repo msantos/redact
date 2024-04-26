53bf367005a2ce83d73dba4833450b5416e40dc5
Michael Santos <michael.santos@gmail.com>
2024-04-26 06:39:43 -0400

    overwrite: remove unused func

--- pkg/redact/overwrite/overwrite.go
+++ pkg/redact/overwrite/overwrite.go
@@ -22,11 +22,3 @@ func (r *Redact) Replace(s string) string {
 func (m *Mask) Replace(s string) string {
 	return strings.Repeat(string(m.Char), len(s))
 }
-
-func (r *Redact) String() string {
-	return "redact"
-}
-
-func (m *Mask) String() string {
-	return "mask"
-}
