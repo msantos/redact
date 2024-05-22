46ebeb9b382a8fd19e1328964771989731adfbd5
Michael Santos <michael.santos@gmail.com>
2024-05-22 07:25:40 -0400

    test: redact: add unmasked example

--- pkg/redact/example_test.go
+++ pkg/redact/example_test.go
@@ -37,3 +37,19 @@ regex = '''\$(?:[a-zA-Z0-9]+)\$([^\s:]+)'''
 	fmt.Println(redacted)
 	// Output: root:$6$*******************************************************************************************************:18515:0:99999:7:::
 }
+
+func ExampleOpt_Redact_unmasked() {
+	red := redact.New(redact.WithRules(`[[rules]]
+id = "crypt-password-hash"
+description = "Detected a password hash"
+regex = '''\$(?:[a-zA-Z0-9]+)\$([^\s:]+)'''
+`),
+		redact.WithOverwrite(&overwrite.Mask{Char: byte('*'), Unmasked: 10}),
+	)
+	redacted, err := red.Redact("root:$6$d468dc01f1cd655d$1c0a188389f4db6399265080815ac488ea65c3295a18d2d7da3ce5e8ef082362adeedec9b69.9704d4d188:18515:0:99999:7:::")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(redacted)
+	// Output: root:$6$d468d*********************************************************************************************4d188:18515:0:99999:7:::
+}
